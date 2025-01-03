212a4db9926e5fded3fdb609afb1694f1c7b5c17
Jacques Bourquin <jacques@jacoscript.com>
2025-01-03 15:55:11 -0600

    Added keystone user/group

---
+++ auth.sh
@@ -0,0 +1,6 @@
+# Create database variables for keystone
+export KEYSTONE_DB=keystone
+export KEYSTONE_DBUSER=keystone
+export KEYSTONE_DBPASS='a&7G!jKs8#Qp'
+export KEYSTONE_USER=keystone
+export KEYSTONE_GROUP=keystone
