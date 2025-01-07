152e72f135a213c65dae21123cfc21f42aac3e6f
Jacques Bourquin <jacques@jacoscript.com>
2025-01-07 02:28:32 -0600

    Added region and keystone admin password

--- auth.sh
+++ auth.sh
@@ -4,3 +4,5 @@ export KEYSTONE_DBUSER=keystone
 export KEYSTONE_DBPASS='a&7G!jKs8#Qp'
 export KEYSTONE_USER=keystone
 export KEYSTONE_GROUP=keystone
+export KEYSTONE_ADMIN_PASS='f7UnnPso3Z7xS1Q8'
+export REGION=RegionOne
